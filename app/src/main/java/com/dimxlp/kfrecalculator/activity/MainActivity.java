0f1c01b5c2a7e16fa182fd8ae2a9888b8b41b8b6
DimXlp <dchlop15@gmail.com>
2025-02-05 23:20:00 +0200

    input fragment, new colors and fonts

--- app/src/main/java/com/dimxlp/kfrecalculator/activity/MainActivity.java
+++ app/src/main/java/com/dimxlp/kfrecalculator/activity/MainActivity.java
@@ -1,18 +1,47 @@
 package com.dimxlp.kfrecalculator.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.dimxlp.kfrecalculator.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "RAFI|MainActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: MainActivity started");
 
+        setContentView(R.layout.activity_main);
+
+        // Initialize Firebase
         FirebaseApp.initializeApp(this);
+        Log.d(TAG, "Firebase initialized");
+
+        // Ensure the NavHostFragment is retrieved correctly
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            Log.d(TAG, "NavHostFragment found and NavController initialized");
+
+            // Set up Bottom Navigation with Navigation Controller
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+            Log.d(TAG, "BottomNavigationView linked with NavController");
+        } else {
+            Log.e(TAG, "NavHostFragment is NULL - Navigation setup failed");
+        }
     }
 }
--- app/src/main/java/com/dimxlp/kfrecalculator/ui/InputFragment.java
+++ app/src/main/java/com/dimxlp/kfrecalculator/ui/InputFragment.java
@@ -1,66 +1,57 @@
 package com.dimxlp.kfrecalculator.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import com.dimxlp.kfrecalculator.R;
+import com.google.android.material.slider.Slider;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InputFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InputFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "RAFI|InputFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InputFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InputFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InputFragment newInstance(String param1, String param2) {
-        InputFragment fragment = new InputFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public InputFragment() {}
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false);
+        View view = inflater.inflate(R.layout.fragment_input, container, false);
+
+        // Initialize UI components
+        EditText ageInput = view.findViewById(R.id.input_age);
+        Spinner genderSpinner = view.findViewById(R.id.input_gender);
+        Slider egfrSlider = view.findViewById(R.id.input_egfr);
+        Slider acrSlider = view.findViewById(R.id.input_acr);
+        Button calculateButton = view.findViewById(R.id.button_calculate);
+
+        // Add a listener for eGFR slider
+        egfrSlider.addOnChangeListener((slider, value, fromUser) -> {
+            Log.d(TAG, "eGFR value changed: " + value);
+        });
+
+        // Add a listener for ACR slider
+        acrSlider.addOnChangeListener((slider, value, fromUser) -> {
+            Log.d(TAG, "ACR value changed: " + value);
+        });
+
+        // Set default values or listeners if needed
+        calculateButton.setOnClickListener(v -> {
+            String age = ageInput.getText().toString();
+            String gender = genderSpinner.getSelectedItem().toString();
+            var egfr = egfrSlider.getValue();
+            var acr = acrSlider.getValue();
+
+            // TODO: Navigate to ResultsFragment and pass data
+        });
+
+        return view;
     }
 }
