f4c43ee130647da5804fa2b00bb8621ef3cc6627
DimXlp <dchlop15@gmail.com>
2025-02-10 23:50:51 +0200

    login activity

---
+++ app/src/main/java/com/dimxlp/kfrecalculator/activity/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.dimxlp.kfrecalculator.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dimxlp.kfrecalculator.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText inputEmail = findViewById(R.id.inputEmail);
+        EditText inputPassword = findViewById(R.id.inputPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        TextView txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        TextView txtSignup = findViewById(R.id.txtSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                // TODO: Add authentication logic here
+            }
+        });
+
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement forgot password functionality
+            }
+        });
+//
+//        txtSignup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
