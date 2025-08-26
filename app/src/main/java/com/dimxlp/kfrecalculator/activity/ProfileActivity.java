78d395c671c9b2b69cf14b1b7ef1fdbc11ad3948
DimXlp <dchlop15@gmail.com>
2025-08-26 23:38:33 +0300

    Sets up top bar in Profile activity

--- app/src/main/java/com/dimxlp/kfrecalculator/activity/ProfileActivity.java
+++ app/src/main/java/com/dimxlp/kfrecalculator/activity/ProfileActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,11 +16,13 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import com.dimxlp.kfrecalculator.R;
 import com.dimxlp.kfrecalculator.adapter.ProfileViewPagerAdapter;
 import com.dimxlp.kfrecalculator.databinding.ActivityProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
     private ActivityProfileBinding binding;
+    private ImageView appLogo, profileImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +32,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         Log.d(TAG, "Activity created.");
 
-        // Setup Toolbar
-        setSupportActionBar(binding.profileToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTopBarFunctionalities();
 
         // Pass intent extras to the adapter
         ProfileViewPagerAdapter adapter = new ProfileViewPagerAdapter(this);
@@ -41,6 +43,61 @@ public class ProfileActivity extends AppCompatActivity {
         new TabLayoutMediator(binding.profileTabLayout, binding.profileViewPager,
                 (tab, position) -> tab.setText(getTabTitle(position))
         ).attach();
+    }
+
+    private void setTopBarFunctionalities() {
+        appLogo = findViewById(R.id.profileAppLogo);
+        profileImg = findViewById(R.id.profileImg);
+
+        appLogo.setOnClickListener(v -> {
+            Log.d(TAG, "App Logo clicked");
+            Intent intent = new Intent(ProfileActivity.this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        profileImg.setOnClickListener(v -> {
+            Log.d(TAG, "Profile clicked");
+            PopupMenu popup = new PopupMenu(this, v);
+            popup.getMenuInflater().inflate(R.menu.profile_menu, popup.getMenu());
+
+            // Force icons to show using reflection
+            try {
+                java.lang.reflect.Field[] fields = popup.getClass().getDeclaredFields();
+                for (java.lang.reflect.Field field : fields) {
+                    if ("mPopup".equals(field.getName())) {
+                        field.setAccessible(true);
+                        Object menuPopupHelper = field.get(popup);
+                        Class<?> classPopupHelper = Class.forName(menuPopupHelper.getClass().getName());
+                        java.lang.reflect.Method setForceIcons = classPopupHelper.getMethod("setForceShowIcon", boolean.class);
+                        setForceIcons.invoke(menuPopupHelper, true);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error showing menu icons", e);
+            }
+
+            popup.setOnMenuItemClickListener(item -> {
+                int itemId = item.getItemId();
+                if (itemId == R.id.menu_profile) {
+                    Toast.makeText(this, getString(R.string.already_in_profile), Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (itemId == R.id.menu_logout) {
+                    Log.d(TAG, "Logout clicked");
+                    FirebaseAuth.getInstance().signOut();
+
+                    Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                    intent.putExtra("SHOW_LOGOUT_MESSAGE", true);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            });
+
+            popup.show();
+        });
     }
 
     private String getTabTitle(int position) {
